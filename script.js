d5ec1283cc4f5e04fd40cdf0ad44a490cd48b40d
cruzfelipe13A fefe cruz <cruzfelipe585@gmail.com>
2024-06-16 01:45:42 +0000

    adicionada as funcoes de buscar as perguntas e montar as perguntas no html

--- script.js
+++ script.js
@@ -19,5 +19,4 @@ function selecionarAssunto(evento){
    const assunto = evento.target.innerText
    localStorage.setItem("assunto", assunto)
    window.location.href = "./pages/quiz/quiz.html"
-
 }
