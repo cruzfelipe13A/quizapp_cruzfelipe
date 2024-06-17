2de05364042cd4e437f15e491080873a14d2749e
cruzfelipe13A fefe cruz <cruzfelipe585@gmail.com>
2024-06-17 00:39:20 +0000

    adicionada a funcao de validar a resposta do usuario

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -9,6 +9,7 @@ let pontos = 0
 let pergunta = 1
 let resposta = ""
 let idInputResposta = ""
+let respostaCorretaId = ""
 
 botaoTema.addEventListener("click", () => {
     trocarTema(body, botaoTema)
@@ -71,7 +72,7 @@ function montarPergunta(){
                     </label>
 
                     <label for="alternativa_b">
-                        <input type="radio" id="alternativa_b" name="alternativa" value= " ${alterarSinais(quiz.questions[pergunta-1].options[1])}">
+                        <input type="radio" id="alternativa_b" name="alternativa" value= "${alterarSinais(quiz.questions[pergunta-1].options[1])}">
 
                         <div>
                             <span>B</span>
@@ -80,7 +81,7 @@ function montarPergunta(){
                     </label>
 
                     <label for="alternativa_c">
-                        <input type="radio" id="alternativa_c" name="alternativa" value= " ${alterarSinais(quiz.questions[pergunta-1].options[2])}">
+                        <input type="radio" id="alternativa_c" name="alternativa" value= "${alterarSinais(quiz.questions[pergunta-1].options[2])}">
 
                         <div>
                             <span>C</span>
@@ -98,7 +99,7 @@ function montarPergunta(){
                     </label>
                 </form>
 
-                <button>Enviar</button>
+                <button>Responder</button>
             </section>
         
     `
@@ -110,9 +111,23 @@ function alterarSinais(texto){
 
 function guardarResposta(evento){
     resposta = evento.target.value
-    idInputResposta = evento.target.id
-
+    idInputResposta = evento.target.id 
+   
+    const botaoEnviar = document.querySelector(".alternativas button")
+    botaoEnviar.addEventListener("click", validarResposta)
 }
+
+function validarResposta() {
+    if (resposta === quiz.questions[pergunta-1].answer) {
+        document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "correta")
+        pontos = pontos + 1
+    } else{
+        document.querySelector(`label[for='${idInputResposta}']`).setAttribute("id", "errada")
+        document.querySelector(`label[for='${respostaCorretaId}']`).setAttribute("id", "correta")
+    }
+}
+
+
 
 async function iniciar() {
     alterarAssunto()
@@ -122,6 +137,10 @@ async function iniciar() {
     const inputsResposta = document.querySelectorAll(".alternativas input")
     inputsResposta.forEach(input =>{
         input.addEventListener("click", guardarResposta)
+
+        if (input.value === quiz.questions[pergunta-1].answer) {
+            respostaCorretaId = input.id 
+        }
     })
 }
 
